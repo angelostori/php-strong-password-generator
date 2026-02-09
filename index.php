896d189853e5e3ef8619e6b32edee4e110653c17
angelostori <angelostori13@gmail.com>
2026-02-09 18:40:59 +0100

    add password generation function and improve form layout in index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+var_dump($_GET);
+
+function pass_generator(int $num) : string {
+    $characters = "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM1234567890,.!£$%&/()^+;:@#";
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,20 +14,32 @@
     <title>Strong Pass Gen</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
-<body>
+<body class="bg-dark">
     <section class="container mt-5">
-    <h1 class="text-center mb-4">Password Generator</h1>
+    <h1 class="text-center mb-4 text-secondary">Strong Password Generator</h1>
 
-    <h5 class="text-center text-muted mb-4">
-        Inserisci da quanti caratteri vuoi che sia formata la tua password sicura
-    </h5>
+    <h2 class="text-center text-light mb-4">
+        Genera una password sicura
+    </h2>
 
-    <form action="" method="GET" class="card p-4 mx-auto shadow-sm bg-light" style="max-width: 400px;">
+    <form action="" method="GET" class="card p-4 mx-auto shadow-sm bg-light">
         <div class="mb-3 text-center">
             <label for="length" class="form-label">
                 Minimo 6 / Massimo 16
             </label>
             <input type="number" name="pass_length" id="length" min="6" max="16" class="form-control text-center">
+        
+            
+            <span>Consenti ripetizioni di uno o più caratteri:</span>
+
+            <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="repeat" id="yes" value="yes">
+            <label class="form-check-label" for="yes">Sì</label>
+            </div>
+            <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="repeat" id="no" value="no">
+            <label class="form-check-label" for="no">No</label>
+            </div>
         </div>
     </form>
 </section>
