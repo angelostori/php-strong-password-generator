8d4f731b27f146ba78255a62d17848bd6103ee30
angelostori <angelostori13@gmail.com>
2026-02-10 20:15:06 +0100

    correct password display logic and improve HTML structure in password.php

--- password.php
+++ password.php
@@ -2,16 +2,26 @@
 
 session_start();
 
-var_dump($_SESSION["password"])
+$password = $_SESSION["password"];
+$pass_length = strlen($password)
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Password</title>
+    <title>Password Generata</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
-<body>
-    <h1>La tua password di X caratteri è: *********</h1>
+<body class="bg-dark">
+
+    <section class="container mt-5">
+        <div class="card bg-light p-3">
+            <?php echo "<h1>La tua password di $pass_length caratteri è: $password</h1>" ?>
+            <a href="./index.php" class="btn btn-primary mx-auto col-2 mt-3"><| Indietro</a>
+        </div>
+    </section>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
